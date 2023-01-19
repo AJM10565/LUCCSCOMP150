4f2b1e08a542da3ef72fa0a343eea3788d4717ce
AJM10565 <32456394+AJM10565@users.noreply.github.com>
2023-01-18 22:34:02 -0600

    added homework1template and lecture 1

---
+++ src/Lectures/01_17_23.py
@@ -0,0 +1,30 @@
+"""
+This is a comment
+About Me(Allan Miller)
+Course Syllabus
+General Thoughts on Computing in the Abstract
+Variables
+Types
+Boolean Logic
+Naming
+"""
+
+
+
+allanmiller = 2
+the_type_of_allanmiller_is=type(allanmiller)
+print(f"{the_type_of_allanmiller_is=}")
+print(type(allanmiller))
+# print("Hello World!")
+message = "Hello World!"
+blah = "hi"
+# print("blah is", type(blah),sep='2',end='')
+print(f"{blah=}")
+# print(blah)
+print(message)
+print(allanmiller)
+
+
+print(f"{bool(0)=}")
+
+
