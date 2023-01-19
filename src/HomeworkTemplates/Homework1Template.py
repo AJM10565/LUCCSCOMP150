41aef0d1e044ecf37565966cccd81e38cabf603b
AJM10565 <32456394+AJM10565@users.noreply.github.com>
2023-01-19 00:36:44 -0600

    fixed the indentation

--- src/HomeworkTemplates/Homework1Template.py
+++ src/HomeworkTemplates/Homework1Template.py
@@ -1,90 +1,103 @@
 def problem4():
-"""
-Your solution to problem 4 goes here. Here's problem statement as a reference:
-Greet the User with user input:
-- Write a program that prompts the user for their name and then greets them by name.
-- Your program should use the input function to get the user's name as a string.
-- You should then use the print function to greet the user by name.
-"""
-  pass
+    """
+    Your solution to problem 4 goes here. Here's problem statement as a reference:
+    Greet the User with user input:
+    - Write a program that prompts the user for their name and then greets them by name.
+    - Your program should use the input function to get the user's name as a string.
+    - You should then use the print function to greet the user by name.
+    """
+    pass
+
 
 def problem5():
-"""
-Your solution to problem 5 goes here. Here's problem statement as a reference:
-Temperature conversion
-- Write a program that converts temperatures from Fahrenheit to Celsius.
-- Your program should prompt the user for a temperature in Fahrenheit.
-- You should then use the appropriate formula to convert the temperature to Celsius.
-- Your program should print the temperature in Celsius to the user.
-"""
-  pass
+    """
+    Your solution to problem 5 goes here. Here's problem statement as a reference:
+    Temperature conversion
+    - Write a program that converts temperatures from Fahrenheit to Celsius.
+    - Your program should prompt the user for a temperature in Fahrenheit.
+    - You should then use the appropriate formula to convert the temperature to Celsius.
+    - Your program should print the temperature in Celsius to the user.
+    """
+    pass
+
 
 def problem6():
-"""
-Your solution to problem 6 goes here. Here's problem statement as a reference:
-Odd or Even
-- Write a program that determines whether a number is odd or even.
-- Your program should prompt the user for a number.
-- You should then use an if statement to check whether the number is odd or even.
-- Your program should print "odd" if the number is odd and "even" if the number is even.
+    """
+    Your solution to problem 6 goes here. Here's problem statement as a reference:
+    Odd or Even
+    - Write a program that determines whether a number is odd or even.
+    - Your program should prompt the user for a number.
+    - You should then use an if statement to check whether the number is odd or even.
+    - Your program should print "odd" if the number is odd and "even" if the number is even.
 
-"""
-  pass
+    """
+    pass
+
 
 def problem7():
-"""
-Your solution to problem 7 goes here. Here's problem statement as a reference:
-Circle Area Calculator
-- Write a program that calculates the area of a circle.
-- The formula for calculating the area of a circle is pi * radius^2.
-- Your program should prompt the user for the radius of the circle.
-- You should then use the appropriate formula to calculate the area of the circle.
-- Your program should print the result of the calculation.
-"""
-  pass
+    """
+    Your solution to problem 7 goes here. Here's problem statement as a reference:
+    Circle Area Calculator
+    - Write a program that calculates the area of a circle.
+    - The formula for calculating the area of a circle is pi * radius^2.
+    - Your program should prompt the user for the radius of the circle.
+    - You should then use the appropriate formula to calculate the area of the circle.
+    - Your program should print the result of the calculation.
+    """
+    pass
+
 
 def problem8():
-"""
-Your solution to problem 8 goes here. Here's problem statement as a reference:
-Greeting Generator
-- Write a program that generates a greeting based on the time of day.
-- Your program should prompt the user for the current time in hours (from 0 to 23).
-- If the time is from 0 to 11, your program should print "Good morning."
-- If the time is from 12 to 17, your program should print "Good afternoon."
-- If the time is from 18 to 23, your program should print "Good evening."
-- Be sure to use proper syntax and punctuation in your program.
-"""
-  pass
+    """
+    Your solution to problem 8 goes here. Here's problem statement as a reference:
+    Greeting Generator
+    - Write a program that generates a greeting based on the time of day.
+    - Your program should prompt the user for the current time in hours (from 0 to 23).
+    - If the time is from 0 to 11, your program should print "Good morning."
+    - If the time is from 12 to 17, your program should print "Good afternoon."
+    - If the time is from 18 to 23, your program should print "Good evening."
+    - Be sure to use proper syntax and punctuation in your program.
+    """
+    pass
+
 
 def problem9():
-"""
-Your solution to problem 9 goes here. Here's problem statement as a reference:
-Write a program that calculates a person's age in years, months, and days.
-- Your program should prompt the user for their birth year, birth month, and birth day.
-- You should then use the current date to calculate the user's age in years, months, and days.
-- Your program should print the user's age in the following format: "Your age is X years, Y months, and Z days."
-- Be sure to use proper syntax and punctuation in your program.
+    """
+    Your solution to problem 9 goes here. Here's problem statement as a reference:
+    Write a program that calculates a person's age in years, months, and days.
+    - Your program should prompt the user for their birth year, birth month, and birth day.
+    - You should then use the current date to calculate the user's age in years, months, and days.
+    - Your program should print the user's age in the following format: "Your age is X years, Y months, and Z days."
+    - Be sure to use proper syntax and punctuation in your program.
 
-"""
-  pass
+    """
+    pass
+
 
 def problem10():
-"""
-Your solution to problem 10 goes here. Here's problem statement as a reference:
-Rock, Paper, Scissors Game
-- Write a program that allows two players to play the rock, paper, scissors game.
-- Your program should prompt each player for their choice (rock, paper, or scissors).
-- You should then determine the winner of the game based on the following rules:
-  - Rock beats scissors.
-  - Scissors beats paper.
-  - Paper beats rock.
-- Your program should print the winner of the game.
-- If the players choose the same option, your program should print, "It's a tie!"
-- Be sure to use proper syntax and punctuation in your program.
-"""
-  pass
+    """
+    Your solution to problem 10 goes here. Here's problem statement as a reference:
+    Rock, Paper, Scissors Game
+    - Write a program that allows two players to play the rock, paper, scissors game.
+    - Your program should prompt each player for their choice (rock, paper, or scissors).
+    - You should then determine the winner of the game based on the following rules:
+      - Rock beats scissors.
+      - Scissors beats paper.
+      - Paper beats rock.
+    - Your program should print the winner of the game.
+    - If the players choose the same option, your program should print, "It's a tie!"
+    - Be sure to use proper syntax and punctuation in your program.
+    """
+    pass
 
 
 if __name__ == "__main__":
-  print("Welcome to Homework")
-  
+    print("Welcome to Homework")
+    # problem4()
+    # problem5()
+    # problem6()
+    # problem7()
+    # problem8()
+    # problem9()
+    # problem10()
+
