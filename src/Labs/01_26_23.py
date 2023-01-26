743216dca9bfac5dc90044c56c7a3e5d0ac16114
AJM10565 <32456394+AJM10565@users.noreply.github.com>
2023-01-26 17:06:24 -0600

    adding solution from class to lab 2

--- src/Labs/01_26_23.py
+++ src/Labs/01_26_23.py
@@ -9,9 +9,30 @@ Finding all the primes
 def primes(number: int):
     """
     print all the primes up to number
+    from lowest to highest
     """
+    # Notably 1 isn't prime, so we will start with 2
+    # also, range needs +1 to try the last number
+
+    for each_number in range(2,number+1):
+        #     print(f"{each_number=}")
+        #     is_it_prime(each_number)
+        if is_it_prime(each_number):
+            # print(is_it_prime(number))
+            print(each_number)
+        # if each_number is prime, then print in
+
 
     pass
+
+def is_it_prime(number: int) -> bool:
+    number_is_prime = True
+    # we need to check if the number we get is divisible by each number less than it.
+    for each_smaller_number in range(2,number):
+        if number % each_smaller_number == 0:
+            number_is_prime = False
+    return number_is_prime
+
 
 
 def word_guessing_game():
