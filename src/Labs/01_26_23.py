b30a527d725fbe8701844d869121d9d0e58f2b52
AJM10565 <32456394+AJM10565@users.noreply.github.com>
2023-01-26 18:36:34 -0600

    added the solutions

--- src/Labs/01_26_23.py
+++ src/Labs/01_26_23.py
@@ -14,7 +14,7 @@ def primes(number: int):
     # Notably 1 isn't prime, so we will start with 2
     # also, range needs +1 to try the last number
 
-    for each_number in range(2,number+1):
+    for each_number in range(2, number + 1):
         #     print(f"{each_number=}")
         #     is_it_prime(each_number)
         if is_it_prime(each_number):
@@ -22,17 +22,16 @@ def primes(number: int):
             print(each_number)
         # if each_number is prime, then print in
 
-
     pass
+
 
 def is_it_prime(number: int) -> bool:
     number_is_prime = True
     # we need to check if the number we get is divisible by each number less than it.
-    for each_smaller_number in range(2,number):
+    for each_smaller_number in range(2, number):
         if number % each_smaller_number == 0:
             number_is_prime = False
     return number_is_prime
-
 
     # return True
 
@@ -89,6 +88,7 @@ def word_guessing_game():
             print(f"You guessed all the letters in the word: {chosen_word}. You win!")
             game_engine = False
 
+
 def word_guessing_game_for_loop():
     words_list = ["car", "bar", "star", "barperson"]
     chosen_word = words_list[3]
@@ -127,6 +127,7 @@ def word_guessing_game_for_loop():
         if "_" not in word_presentation:
             print(f"You guessed all the letters in the word: {chosen_word}. You win!")
             break
+
 
 if __name__ == "__main__":
     print("Welcome to the Lab")
