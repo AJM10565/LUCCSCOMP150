7ce1eae8538213c9df903d15f633ebe876ca562f
AJM10565 <32456394+AJM10565@users.noreply.github.com>
2023-01-31 17:44:25 -0600

    added beginnings of homework #3

---
+++ src/HomeworkTemplates/Homework3Template.py
@@ -0,0 +1,30 @@
+"""
+Homework #2 Instructions:
+Submit final solution on sakai. Use this file as a template, rename according to the class convention: UVIDHomework3.py
+To the best of your ability complete problems 1,2,3,4, and 5. If you use a resource, be sure to leave a link inside the
+function documentation for that problem.
+"""
+
+
+def problem1(coordinates: str) -> str:
+    """
+    Write a Python function that checks if a completed sudoku board is correct
+    Sudoku boards are traditionally 9x9 but your code should take in a list of arbitrary length
+    check if the list is perfect square, and if it is, check if the list of numbers is a correct solution
+
+    """
+
+def problem2():
+    """
+    Write a python function that produces a 9 x 9  sudoku board,
+
+
+
+    :return:
+    """
+
+
+if __name__ == "__main__":
+    """ Uncomment one of these to run/test that problem"""
+    # problem1()
+
---
+++ src/Lectures/01_31_23.py
@@ -0,0 +1,11 @@
+"""
+Agenda:
+- sakai Research Paper topic submission due 1 week from today
+- Review Homework #2 Solutions
+- Review Quiz #4 Solutions
+
+simple data structures:
+strings: "This is a string"
+lists: ["this","is","a","list"]
+dictionaries: {"Key":"Value"}
+"""
